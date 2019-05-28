4380bec6018f2f7468052d71525cb6d7bb1b919a
guzziye <13683237@qq.com>
2019-05-28 19:21:34 +0800

    commit

--- src/kongyixueyuan.com/publicChain/Serialize-DeserializeBlock/BLC/blockchain.go
+++ src/kongyixueyuan.com/publicChain/Serialize-DeserializeBlock/BLC/blockchain.go
@@ -15,7 +15,7 @@ const blocksBucket = "blocks"
 type Blockchain struct {
 	//Blocks []*Block //存储有序的区块
 	Tip []byte   //区块键里最后一个区块的Hash
-	DB  *bolt.BD //数据库
+	DB  *bolt.DB //数据库
 }
 
 //新增区块
@@ -26,12 +26,17 @@ func (blockchain *Blockchain) AddBlock(data string) {
 	//
 	////2 加到blockchain里面
 	//blockchain.Blocks = append(blockchain.Blocks, newBlock)
+
+	//=====================================================
+
+
+
 }
 
 //创建一个带有创或区块的区块键
 func NewBlockchain() *Blockchain {
 
-	var
+	var tip [] byte //获取最后一个区块hash
 	//return &Blockchain{[]*Block{NewGenesisBlock()}}
 	//1.尝试打开或是创建数据库
 	//如果数据存在 就打开，否则创建一个数据库
@@ -39,6 +44,7 @@ func NewBlockchain() *Blockchain {
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	//2 db.update更新数据
 	//2.1 表是否存在，如果不存在需要创建表
 	err = db.Update(func(tx *bolt.Tx) error {
@@ -48,9 +54,28 @@ func NewBlockchain() *Blockchain {
 		//表不存
 		if b == nil {
 			fmt.Println(" No existing blockchain found. create a new one.")
+			//创建创世区块
+			genesisBlock := NewGenesisBlock()
+			//创建表
+			b, err = tx.CreateBucket([]byte(blocksBucket))
+			if err != nil {
+				log.Panic(err)
+			}
+			//创世区块序列之后的数据存储到表中
+			err =b.Put(genesisBlock.Hash,genesisBlock.Serialize())
+			if err != nil {
+				log.Panic(err)
+			}
 
-		} else {
-			//表存在
+			err =b.Put([]byte("L"),genesisBlock.Hash)
+			if err != nil {
+				log.Panic(err)
+			}
+
+			tip = genesisBlock.Hash
+		} else { //表存在
+			//KEY:L ,value为最后一个区块hash
+			tip = b.Get([]byte("L"))
 		}
 		return nil
 	})
@@ -62,4 +87,5 @@ func NewBlockchain() *Blockchain {
 	//2.3 创世区块的hash作为key block序列化数据做为value 存在表中
 	//2.4 设置一个key L,将hash作为value存进数据库中。作为最后一个 区块
 
+	return &Blockchain{tip, db}
 }
--- src/kongyixueyuan.com/publicChain/Serialize-DeserializeBlock/main.go
+++ src/kongyixueyuan.com/publicChain/Serialize-DeserializeBlock/main.go
@@ -3,36 +3,14 @@ package main
 import (
 	"fmt"
 	"kongyixueyuan.com/publicChain/Serialize-DeserializeBlock/BLC"
-	"time"
-)
-
-const (
-	date        = "2006-01-02"
-	shortdate   = "06-01-02"
-	times       = "15:04:02"
-	shorttime   = "15:04"
-	datetime    = "2006-01-02 15:04:02"
-	newdatetime = "2006/01/02 15时04分02秒"
-	newtime     = "15~04~02"
 )
 
 func main() {
+  blockchain:= BLC.NewBlockchain()
 
-	blockchain := BLC.NewBlockchain()
-	blockchain.AddBlock("send ben 20 btc")
-	blockchain.AddBlock("send ben -> bobi 50 btc")
-	blockchain.AddBlock("send ben -> bobi 5 btc")
-	blockchain.AddBlock("send ben -> bobi 6 btc")
-	blockchain.AddBlock("send ben -> bobi 7 btc")
-	blockchain.AddBlock("send ben -> bobi 8 btc")
-	blockchain.AddBlock("send ben -> bobi 9 btc")
+  fmt.Println(blockchain)
+  fmt.Printf("tip: %x\n",blockchain.Tip)
 
-	fmt.Println("=======start block========")
-	//	fmt.Println( blockchain)
-	for _, block := range blockchain.Blocks {
-		fmt.Printf("Data:%s ,PreBlockHash:%x ,Hash:%x,Timestamp:%v \n", block.Data, block.PrevBlockHash, block.Hash, time.Unix(block.Timestamp, 0).Format(newdatetime))
-		fmt.Printf("Hash  :%x \n", block.Hash)
 
-		fmt.Printf("Nonce :%b \n", block.Nonce)
-	}
+
 }
